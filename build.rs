eb2688ca8ac50ce2e7e6591a5f0f2e995fc1e5aa
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-11-20 09:37:35 +0100

    Simplify the build script

--- build.rs
+++ build.rs
@@ -6,52 +6,33 @@ use std::fs::metadata;
 use std::path::PathBuf;
 use std::process::Command;
 
+macro_rules! switch(($condition:expr) => (if $condition { "ON" } else { "OFF" }));
+
 fn main() {
-    if var("CARGO_FEATURE_SYSTEM_NETLIB").is_ok() {
-        use_system();
-    } else {
-        use_bundled();
-    }
-}
-
-fn use_system() {
     let kind = if var("CARGO_FEATURE_STATIC_NETLIB").is_ok() { "static" } else { "dylib" };
     let cblas = var("CARGO_FEATURE_EXCLUDE_CBLAS").is_err();
     let lapacke = var("CARGO_FEATURE_EXCLUDE_LAPACKE").is_err();
 
-    println!("cargo:rustc-link-lib={}=blas", kind);
-    println!("cargo:rustc-link-lib={}=lapack", kind);
-    println!("cargo:rustc-link-lib=dylib=gfortran");
-    if cblas {
-        println!("cargo:rustc-link-lib={}=cblas", kind);
-    }
-    if lapacke {
-        println!("cargo:rustc-link-lib={}=lapacke", kind);
-    }
-}
+    if !var("CARGO_FEATURE_SYSTEM_NETLIB").is_ok() {
+        let source = PathBuf::from(&var("CARGO_MANIFEST_DIR").unwrap()).join("source");
 
-fn use_bundled() {
-    let kind = if var("CARGO_FEATURE_STATIC_NETLIB").is_ok() { "static" } else { "dylib" };
-    let cblas = var("CARGO_FEATURE_EXCLUDE_CBLAS").is_err();
-    let lapacke = var("CARGO_FEATURE_EXCLUDE_LAPACKE").is_err();
+        if metadata(&source.join("CBLAS/CMAKE")).is_err() {
+            let _ = Command::new("ln")
+                            .args(&["-s", "cmake", "CMAKE"])
+                            .current_dir(&source.join("CBLAS"))
+                            .status();
+        }
 
-    let source = PathBuf::from(&var("CARGO_MANIFEST_DIR").unwrap()).join("source");
+        let output = Config::new(&source)
+                            .define("BUILD_TESTING", "OFF")
+                            .define("BUILD_SHARED_LIBS", switch!(kind == "dylib"))
+                            .define("CBLAS", switch!(cblas))
+                            .define("LAPACKE", switch!(lapacke))
+                            .build();
 
-    if metadata(&source.join("CBLAS/CMAKE")).is_err() {
-        let _ = Command::new("ln")
-                        .args(&["-s", "cmake", "CMAKE"])
-                        .current_dir(&source.join("CBLAS"))
-                        .status();
+        println!("cargo:rustc-link-search={}", output.join("lib").display());
     }
 
-    let output = Config::new(&source)
-                        .define("BUILD_TESTING", "OFF")
-                        .define("BUILD_SHARED_LIBS", if kind == "dylib" { "ON" } else { "OFF" })
-                        .define("CBLAS", if cblas { "ON" } else { "OFF" })
-                        .define("LAPACKE", if lapacke { "ON" } else { "OFF" })
-                        .build();
-
-    println!("cargo:rustc-link-search={}", output.join("lib").display());
     println!("cargo:rustc-link-lib={}=blas", kind);
     println!("cargo:rustc-link-lib={}=lapack", kind);
     println!("cargo:rustc-link-lib=dylib=gfortran");
