9c1b23429d2e5793226091c1756ffd830e76d04b
Ivan Ukhov <ivan.ukhov@gmail.com>
2017-03-18 19:10:08 +0100

    Add a suffix to the names of blas and lapack

--- build.rs
+++ build.rs
@@ -1,29 +1,24 @@
 extern crate cmake;
 
 use cmake::Config;
-use std::env::var;
-use std::fs::metadata;
-use std::path::PathBuf;
-use std::process::Command;
+use std::{env, fs, os};
+use std::path::{Path, PathBuf};
 
-macro_rules! feature(($name:expr) => (var(concat!("CARGO_FEATURE_", $name)).is_ok()));
+macro_rules! feature(($name:expr) => (env::var(concat!("CARGO_FEATURE_", $name)).is_ok()));
 macro_rules! switch(($condition:expr) => (if $condition { "ON" } else { "OFF" }));
 
 fn main() {
+    let mut suffix = "";
     let kind = if feature!("STATIC") { "static" } else { "dylib" };
     let cblas = feature!("CBLAS");
     let lapacke = feature!("LAPACKE");
-
     if !feature!("SYSTEM") {
+        suffix = "-netlib";
         let source = PathBuf::from("source");
-
-        if metadata(&source.join("CBLAS/CMAKE")).is_err() {
-            let _ = Command::new("ln")
-                            .args(&["-s", "cmake", "CMAKE"])
-                            .current_dir(&source.join("CBLAS"))
-                            .status();
+        if !source.join("CBLAS").join("CMAKE").exists() {
+            os::unix::fs::symlink(source.join("CBLAS").join("cmake"),
+                                  source.join("CBLAS").join("CMAKE")).unwrap();
         }
-
         let output = Config::new(&source)
                             .define("BUILD_TESTING", "OFF")
                             .define("BUILD_SHARED_LIBS", switch!(kind == "dylib"))
@@ -31,12 +26,13 @@ fn main() {
                             .define("LAPACKE_WITH_TMG", switch!(lapacke))
                             .define("CMAKE_INSTALL_LIBDIR", "lib")
                             .build();
-
-        println!("cargo:rustc-link-search={}", output.join("lib").display());
+        let output = output.join("lib");
+        rename(&output, "blas", suffix);
+        rename(&output, "lapack", suffix);
+        println!("cargo:rustc-link-search={}", output.display());
     }
-
-    println!("cargo:rustc-link-lib={}=blas", kind);
-    println!("cargo:rustc-link-lib={}=lapack", kind);
+    println!("cargo:rustc-link-lib={}=blas{}", kind, suffix);
+    println!("cargo:rustc-link-lib={}=lapack{}", kind, suffix);
     println!("cargo:rustc-link-lib=dylib=gfortran");
     if cblas {
         println!("cargo:rustc-link-lib={}=cblas", kind);
@@ -44,5 +40,23 @@ fn main() {
     if lapacke {
         println!("cargo:rustc-link-lib={}=lapacke", kind);
         println!("cargo:rustc-link-lib={}=tmglib", kind);
+    }
+}
+
+fn rename(directory: &Path, name: &str, suffix: &str) {
+    for entry in fs::read_dir(directory).unwrap() {
+        let path = entry.unwrap().path();
+        let stem = path.file_stem().unwrap().to_str().unwrap();
+        if !stem.starts_with("lib") {
+            continue;
+        }
+        if &stem[3..] != name {
+            continue;
+        }
+        let mut new_path = path.clone();
+        new_path.set_file_name(format!("lib{}{}", name, suffix));
+        new_path.set_extension(path.extension().unwrap());
+        fs::rename(&path, &new_path).unwrap();
+        return;
     }
 }
